65182cbeeee7a15f2a2b8b9727c123115f3aab93
Millena Fernandes <107659330+Millenafernandes@users.noreply.github.com>
2023-03-02 08:32:20 -0300

    Add files via upload

---
+++ aula07.java
@@ -0,0 +1,29 @@
+package javaAula;
+
+public class aula07 {
+	
+	public static void main(String[] args) {
+		
+		int vetor[] = new int [10];
+		
+		vetor[0]= 16;
+		vetor[1]= 22;
+		vetor[2]= 50;
+		vetor[3]= 86;
+		vetor[4]= 21;
+		vetor[5]= 21;
+		vetor[6]= 21;
+		vetor[7]= 21;
+		vetor[8]= 21;
+		vetor[9]= 21;
+		
+	
+		for(int i = 0; i <= vetor.length; i++ ) {
+		
+		
+		System.out.println(vetor[i]);
+		
+		}
+	}
+
+}
